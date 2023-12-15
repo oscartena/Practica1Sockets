f8c6b2808e22b48da2a350f51b0ce497086501f2
oscmar <oscar.tena.marin@gmail.com>
2023-12-15 16:56:01 +0100

    Initial commit

---
+++ UDPTimeServer.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+public class UDPTimeServer {
+
+    public static void main(String args[]) {
+
+        try {
+
+            DatagramSocket socketUDP = new DatagramSocket(6789);
+            byte[] buffer = new byte[1000];
+
+            while (true) {
+                // Construimos el DatagramPacket para recibir peticiones
+                DatagramPacket receivedDatagram = new DatagramPacket(buffer, buffer.length);
+
+                socketUDP.receive(receivedDatagram);
+
+                System.out.print("Datagrama recibido del host: " + receivedDatagram.getAddress());
+                System.out.println(" desde el puerto remoto: " + receivedDatagram.getPort());
+
+                String received = new String(receivedDatagram.getData()).substring(0, 4);
+                if (received.compareTo("time") == 0) {
+                    String time = String.format("%02d:%02d", LocalDateTime.now().getHour(), LocalDateTime.now().getMinute());
+                    System.out.printf("Sending response: %s", time);
+                    DatagramPacket responseDatagram = new DatagramPacket(time.getBytes(), time.length(), receivedDatagram.getAddress(), receivedDatagram.getPort());
+                    socketUDP.send(responseDatagram);
+                }
+            }
+
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        }
+    }
+
+}
